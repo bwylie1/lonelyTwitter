3c152508e8c1f90a001cb13e16d1d3b2432e5759
bwylie1 <bwylie@ualberta.ca>
2014-01-16 14:57:37 -0700

    Constructors added

--- src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
+++ src/ca/ualberta/cs/lonelytwitter/LonelyTweetModel.java
@@ -6,17 +6,33 @@ public class LonelyTweetModel {
     private String text;
     private Date timestamp;
     
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
+	public LonelyTweetModel(String text) {
+		super();
 		this.text = text;
+		timestamp = new Date();
 	}
-	public Date getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Date timestamp) {
+	
+	public LonelyTweetModel(String text, Date timestamp) {
+		super();
+		this.text = text;
 		this.timestamp = timestamp;
 	}
     
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	
+
 }
